9854da357afa00e928d9955d60b9f8fff032dfcf
VarzikaT <varzikatiwari08@gmail.com>
2021-11-16 10:54:38 +0530

    reverse string method.

---
+++ Stack/src/p/String1.java
@@ -0,0 +1,25 @@
+package p;
+
+import java.util.Arrays;
+import java.util.Stack;
+import java.lang.String;
+
+public class String1 {
+	public static void main(String args[]) {
+		String str = "ABCD";
+		Stack<Character> stack = new Stack<>();
+		char[] chars = str.toCharArray();
+		for( char c : chars) {
+			stack.push(c);
+		}
+		
+		for (int i = 0 ; i< str.length();i++) {
+			chars[i]=stack.pop();
+		}
+		
+		System.out.println(Arrays.toString(chars));
+		str = String.valueOf(chars);
+		System.out.println(str);
+	}
+	
+}
