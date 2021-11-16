45d8eec7205e823461bbc1c3295f4aa8e4a98528
VarzikaT <varzikatiwari08@gmail.com>
2021-11-16 10:38:04 +0530

    added push and pop method.

--- Stack/src/p/Stack.java
+++ Stack/src/p/Stack.java
@@ -1,5 +1,7 @@
 package p;
 
+import java.util.EmptyStackException;
+
 public class Stack {
 	
 	private ListNode top;
@@ -14,4 +16,54 @@ public class Stack {
 			this.next = null;
 		}
 	}
+	
+	public Stack() {
+		top =null;
+		length = 0;
+	}
+	
+	public int length() {
+		return length;
+	}
+	
+	public boolean isEmpty() {
+		return length == 0;
+		
+	}
+	
+	public void push(int data) {
+		ListNode temp = new ListNode(data);
+		temp.next=top;
+		top = temp;
+		length++;
+	}
+	public int peek() {
+		if(isEmpty()) {
+			throw new EmptyStackException();
+		}
+		return top.data;
+	}
+	public int pop() {
+		if(isEmpty()) {
+			throw new EmptyStackException();
+		}
+		int result = top.data;
+		top = top.next;
+		length--;
+		return result;
+		
+	}
+
+public static void main(String args[]) {
+	Stack stack = new Stack();
+	stack.push(10);
+	stack.push(15);
+	stack.push(20);
+	System.out.println(stack.peek());
+	stack.pop();
+	System.out.println(stack.peek());
+	stack.pop();
+	System.out.println(stack.peek());
+	
+}
 }
