154f7958d04eed1a55f3e36bb00cb70f0d1eb079
VarzikaT <varzikatiwari08@gmail.com>
2021-11-30 10:37:59 +0530

    preorder traversal of binary tree added.

--- DSA/src/Tree/Binarytree.java
+++ DSA/src/Tree/Binarytree.java
@@ -36,4 +36,11 @@ public class Binarytree {
 			bt.createBinaryTree();
 		}
 	}
+		public void preOrder(TreeNode root) {
+			if(root==null) {
+				return;
+			}
+			System.out.print(root.data + " ");
+			preOrder(root.left);
+			preOrder(root.right);
 }
