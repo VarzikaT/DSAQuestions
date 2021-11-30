138d361d13fdcc5315119ab58f64fe1b06a59c99
VarzikaT <varzikatiwari08@gmail.com>
2021-11-30 10:25:17 +0530

    adding a constructor method

--- DSA/src/Tree/Binarytree.java
+++ DSA/src/Tree/Binarytree.java
@@ -12,4 +12,23 @@ public class Binarytree {
 			this.data = data;
 		}
 	}
+	public void createBinaryTree() {
+		TreeNode first = new TreeNode(1);
+		TreeNode second = new TreeNode(2);
+		TreeNode third = new TreeNode(3);
+		TreeNode fourth = new TreeNode(4);
+		TreeNode fifth = new TreeNode(5);
+		TreeNode sixth = new TreeNode(6);
+		TreeNode seventh = new TreeNode(7);
+		root=first;
+		first.left = second;
+		first.right = third;
+		second.left = fouth;
+		second.right = fifth;
+		third.left = sixth;
+		third.right = seventh;
+		
+		
+		
+	}
 }
